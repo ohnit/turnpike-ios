e11496fc747daac117b5bc690d0e4f4ff975a67b
James Lawrence Turner <james@jameslawrenceturner.com>
2013-07-05 11:18:35 -0700

    Implemented TPAnonymous filters, which create filters from blocks (rather than having to implement TPFilterProtocol yourself.

--- Turnpike/TPAnonymousFilter.m
+++ Turnpike/TPAnonymousFilter.m
@@ -7,7 +7,31 @@
 //
 
 #import "TPAnonymousFilter.h"
+#import "TPFilterChain.h"
+
+@interface TPAnonymousFilter()
+
+@property (strong, nonatomic) TPFilterBlock filterBlock;
+
+@end
 
 @implementation TPAnonymousFilter
+
++ (id<TPFilterProtocol>) filterWithBlock:(TPFilterBlock)filterBlock {
+    TPAnonymousFilter *filter = [[TPAnonymousFilter alloc]init];
+    filter.filterBlock = filterBlock;
+    return filter;
+}
+
+@synthesize filterBlock=_filterBlock;
+
+- (void) doFilterWithRequest:(TPRouteRequest *)request AndFilterChain:(TPFilterChain *)filterChain {
+    if (self.filterBlock) {
+        self.filterBlock(request, filterChain);
+    }
+    else {
+        [filterChain doFilterWithRequest:request];
+    }
+}
 
 @end
