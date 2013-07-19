897f37754f35d018653ae591852ad96894eb86bc
James Lawrence Turner <james@jameslawrenceturner.com>
2013-07-18 17:38:30 -0700

    Builds with Xcode toolchain.

---
+++ build/Turnpike.build/Release-iphonesimulator/Turnpike.build/Objects-normal/i386/Turnpike.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Users/jlturner/Desktop/Scratchpad/URX/Turnpike/Turnpike/Turnpike.m \
+  /Users/jlturner/Desktop/Scratchpad/URX/Turnpike/Turnpike/Turnpike.h \
+  /Users/jlturner/Desktop/Scratchpad/URX/Turnpike/Turnpike/TPFilterChain.h \
+  /Users/jlturner/Desktop/Scratchpad/URX/Turnpike/Turnpike/TPRouter.h \
+  /Users/jlturner/Desktop/Scratchpad/URX/Turnpike/Turnpike/TPRouteRequest.h \
+  /Users/jlturner/Desktop/Scratchpad/URX/Turnpike/Turnpike/TPAnonymousFilter.h \
+  /Users/jlturner/Desktop/Scratchpad/URX/Turnpike/Turnpike/TPFilterProtocol.h \
+  /Users/jlturner/Desktop/Scratchpad/URX/Turnpike/Turnpike/TPHelper.h
